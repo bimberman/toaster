c5a8c9af832b7909bbde3bf904a798191d9da597
Ben Imberman <ben.imberman1@t-mobile.com>
2024-04-04 08:17:36 -0700

    added additional classes for the heater and slots

--- src/ToastableItem.java
+++ src/ToastableItem.java
@@ -1,15 +1,10 @@
 
-public class ToastableItem {
+public abstract class ToastableItem {
 	boolean isToasted = false;
 	boolean isToasting = false;
-	int toastedAmount = 0;
+	double toastedAmount = 0;
 	int width = 0;
 	int height = 0;
-	
-	ToastableItem(int width, int height){
-		this.width = width;
-		this.height = height;
-	}
 	
 	public void printState() {
 		System.out.println("isToasted: " + isToasted + ", isToasting:" + isToasting + ", toastedAmount:" + toastedAmount);
@@ -18,4 +13,6 @@ public class ToastableItem {
 	public void printState(String s) {
 		System.out.println(s + ": isToasted: " + isToasted + ", isToasting:" + isToasting + ", toastedAmount:" + toastedAmount);
 	}
+	
+	public abstract double getToastedAmountFromIntensity(int intensity);
 }
--- src/ToasterSlot.java
+++ src/ToasterSlot.java
@@ -1,4 +1,67 @@
 
 public class ToasterSlot {
-
+	Heater[] heaters = null;
+	boolean isEngaged = false;
+	int time = 0;
+	int DEFAULT_NUMBER_OF_HEATERS = 1;
+	
+	public ToasterSlot() {
+		this.heaters = new Heater[DEFAULT_NUMBER_OF_HEATERS];
+		
+		for(Heater h: this.heaters) {
+			h = new Heater();
+		}
+	}
+	
+	public ToasterSlot(int numOfHeaters) {
+		this.heaters = new Heater[numOfHeaters];
+		
+		for(Heater h: this.heaters) {
+			h = new Heater();
+		}
+	}
+	
+	public ToasterSlot(int numOfHeaters, int maxHeat) {
+		this.heaters = new Heater[numOfHeaters];
+		
+		for(Heater h: this.heaters) {
+			h = new Heater(maxHeat);
+		}
+	}
+	
+	public ToasterSlot(int numOfHeaters, int maxHeat, int minHeat) {
+		this.heaters = new Heater[numOfHeaters];
+		
+		for(Heater h: this.heaters) {
+			h = new Heater(maxHeat, minHeat);
+		}
+	}
+	
+	public Heater[] getHeaters() {
+		return this.heaters;
+	}
+	
+	public void setIsEnggaged(boolean isEngaged) {
+		this.isEngaged = isEngaged;
+	}
+	
+	public boolean getIsEnggaged() {
+		return this.isEngaged;
+	}
+	
+	public void setTime(int time) {
+		this.time = time;
+	}
+	
+	public int getTime() {
+		return this.time;
+	}
+	
+	public int getIntensity() {
+		int intensity = 0;
+		for(Heater h: this.heaters) {
+			intensity+=time*h.currentHeat;
+		}
+		return intensity;
+	}
 }
